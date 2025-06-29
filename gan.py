05cb933a29a7fec3166d5439902abed800f62d93
ManuelRios18 <manuelrios18@hotmail.com>
2025-06-29 09:19:10 -0500

    Improve code style

--- gan.py
+++ gan.py
@@ -1,17 +1,17 @@
+import os
 import tqdm
 import torch
 import random
 from torch import nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
-from torchvision.utils import make_grid
 from torchvision import datasets, transforms
+from utils import reset_dir, save_grid, plot_learning_curves
 
 Z_DIM = 100
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.linear_stack = nn.Sequential(
             nn.Flatten(),
@@ -24,12 +24,12 @@ class Discriminator(nn.Module):
             nn.Linear(in_features=32, out_features=1)
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear_stack(x)
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.linear_stack = nn.Sequential(
             nn.Linear(in_features=Z_DIM, out_features=32),
@@ -45,13 +45,13 @@ class Generator(nn.Module):
             nn.Tanh()
         )
 
-    def forward(self, z):
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
         x = self.linear_stack(z)
         return x.view(-1, 1, 28, 28)
 
 
 class MNISTImagesSampler:
-    def __init__(self):
+    def __init__(self) -> None:
         transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.5, ),std=(0.5, ))
@@ -65,7 +65,7 @@ class MNISTImagesSampler:
         random.shuffle(self.indices)
         self.position = 0
 
-    def sample(self, n_samples):
+    def sample(self, n_samples: int) -> torch.Tensor:
         if self.position + n_samples > len(self.indices):
             random.shuffle(self.indices)
             self.position = 0
@@ -74,108 +74,91 @@ class MNISTImagesSampler:
         self.position += n_samples
 
         batch = [self.dataset[i] for i in batch_indices]
-        data, targets = zip(*batch)
+        data, _ = zip(*batch)
         return torch.stack(data)
 
-def sample_z(n_samples: int):
+def sample_z(n_samples: int) -> torch.Tensor:
     return torch.normal(mean=0, std=1, size=(n_samples, Z_DIM))
 
 
-x_sampler = MNISTImagesSampler()
-discriminator = Discriminator()
-generator = Generator()
-
-if torch.accelerator.is_available():
-    # device = torch.accelerator.current_accelerator()
+def main() -> None:
+    x_sampler = MNISTImagesSampler()
+    discriminator = Discriminator()
+    generator = Generator()
     device = "cpu"
-else:
-    device = "cpu"
+    logs_path = "training_logs/gan"
+    reset_dir(logs_path)
 
-print(f"Using device: {device}")
+    discriminator.to(device)
+    generator.to(device)
 
-discriminator.to(device)
-generator.to(device)
+    # Generate some initial data to see how an untrained model behaves.
+    z_to_plot = sample_z(n_samples=16).to(device)
 
-# Generate some initial data to see how an untrained model behaves.
-z_to_plot = sample_z(n_samples=16).to(device)
-real_data_sample = x_sampler.sample(n_samples=16).to(device)
-pre_training_generations = generator.forward(z_to_plot).detach().cpu().numpy()
+    batch_size = 64
+    num_iterations = 93750
+    learning_rate = 0.0002
+    optimizer_g = optim.Adam(params=generator.parameters(),
+                             lr=learning_rate,
+                             weight_decay=0)
+    optimizer_d = optim.Adam(params=discriminator.parameters(),
+                             lr=learning_rate,
+                             weight_decay=0)
+    bce_loss = nn.BCEWithLogitsLoss()
+    g_losses, d_losses = [], []
+    for iteration in tqdm.tqdm(range(num_iterations)):
+        # Optimize the discriminator
+        optimizer_d.zero_grad()
+        z = sample_z(n_samples=batch_size).to(device)
+        x = x_sampler.sample(n_samples=batch_size).to(device)
 
+        # Detach tensors to avoid gradient accumulation.
+        generated_x = generator(z).detach()
 
-batch_size = 64
-num_iterations = 93750
-learning_rate = 0.0002
-optimizer_g = optim.Adam(params=generator.parameters(),
-                         lr=learning_rate,
-                         weight_decay=0)
-optimizer_d = optim.Adam(params=discriminator.parameters(),
-                         lr=learning_rate,
-                         weight_decay=0)
-bce_loss = nn.BCEWithLogitsLoss()
-g_losses, d_losses = [], []
-for iteration in tqdm.tqdm(range(num_iterations)):
-    # Optimize the discriminator
-    optimizer_d.zero_grad()
-    z = sample_z(n_samples=batch_size).to(device)
-    x = x_sampler.sample(n_samples=batch_size).to(device)
+        real_predictions = discriminator(x)
+        fake_predictions = discriminator(generated_x)
 
-    # Detach tensors to avoid gradient accumulation on the generator params.
-    generated_x = generator.forward(z).detach()
+        real_loss = bce_loss(real_predictions,
+                             torch.ones_like(real_predictions).to(device))
+        fake_loss = bce_loss(fake_predictions,
+                             torch.zeros_like(fake_predictions).to(device))
+        discriminator_loss = (real_loss + fake_loss) / 2
 
-    d_real_pred = discriminator.forward(x)
-    d_fake_pred = discriminator.forward(generated_x)
+        discriminator_loss.backward()
+        optimizer_d.step()
 
-    real_loss = bce_loss(d_real_pred, torch.ones(batch_size, 1).to(device))
-    fake_loss = bce_loss(d_fake_pred, torch.zeros(batch_size, 1).to(device))
-    discriminator_loss = (real_loss + fake_loss) / 2
+        # Optimize the generator
+        optimizer_g.zero_grad()
+        z = sample_z(n_samples=batch_size).to(device)
+        generated_x = generator(z)
+        fake_predictions = discriminator(generated_x)
+        # According to Goodfellow et al. Minimizing log(1-D(G(z))) can cause
+        # saturation. Therefore, authors suggest to maximize D(G(z)).
+        # This suggestion is equivalent to minimize -D(G(z))
+        # as implemented below
+        generator_loss = bce_loss(fake_predictions,
+                                  torch.ones_like(fake_predictions).to(device))
 
-    discriminator_loss.backward()
-    optimizer_d.step()
+        generator_loss.backward()
+        optimizer_g.step()
+        d_losses.append(discriminator_loss.item())
+        g_losses.append(generator_loss.item())
+        if iteration % 1000 == 0:
+            print(f"Epoch {iteration}: "
+                  f"D_loss -> {discriminator_loss.item():.4f},"
+                  f" G_loss -> {generator_loss.item():.4f}")
+            with torch.no_grad():
+                sampled_images = generator(z_to_plot).detach().cpu().numpy()
+            save_grid(images=sampled_images,
+                      title=f"GAN generation {iteration}",
+                      file_name=os.path.join(logs_path, f"{iteration}.png"))
 
-    # Optimize the generator
-    optimizer_g.zero_grad()
-    z = sample_z(n_samples=batch_size).to(device)
-    generated_x = generator.forward(z)
-    d_fake_pred = discriminator.forward(generated_x)
-    # According to Goodfellow et al. Minimizing log(1-D(G(z))) can cause
-    # saturation. Therefore, authors suggest to maximize D(G(z)).
-    # This suggestion is equivalent to minimize -D(G(z)) as implemented below
-    generator_loss = bce_loss(d_fake_pred,
-                              torch.ones(batch_size, 1).to(device))
-
-    generator_loss.backward()
-    optimizer_g.step()
-    d_losses.append(discriminator_loss.item())
-    g_losses.append(generator_loss.item())
-    if iteration % 1000 == 0:
-        print(f"Epoch {iteration}: D_loss -> {discriminator_loss.item():.4f},"
-              f" G_loss -> {generator_loss.item():.4f}")
-
-post_training_generations = generator.forward(z_to_plot).detach().cpu().numpy()
-
-plt.figure()
-plt.title("Training Losses")
-plt.plot(d_losses, label="Discriminator Loss")
-plt.plot(g_losses, label="Generator Loss")
-plt.legend()
-plt.grid()
-
-
-def show_grid(imgs_np, title):
-
-    imgs = torch.tensor(imgs_np, dtype=torch.float32)
-    grid = make_grid(imgs, nrow=4, padding=2, normalize=True, value_range=(-1, 1))
-    plt.figure(figsize=(4,4))
-    plt.title(title)
-    # grid is (C, H, W); move channels last for imshow
-    plt.imshow(grid.permute(1, 2, 0).cpu().numpy(), cmap='gray')
-    plt.axis('off')
-
-
-show_grid(real_data_sample,
-          title="Real data ")
-show_grid(pre_training_generations,
-          title="Generator output  —  before training")
-show_grid(post_training_generations,
-          title="Generator output  —  after training")
-plt.show()
+    final_generations = generator(z_to_plot).detach().cpu().numpy()
+    save_grid(images=final_generations,
+              title="GAN generation",
+              file_name=os.path.join(logs_path, "post_training.png"))
+    plot_learning_curves(d_losses=d_losses,
+                         g_losses=g_losses,
+                         title="Learning Curves GAN",
+                         file_name=os.path.join(logs_path, "curves.png"))
+main()
--- vanilla_gan.py
+++ vanilla_gan.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.linear_stack = nn.Sequential(
             nn.Linear(in_features=2, out_features=20),
@@ -17,13 +17,13 @@ class Discriminator(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear_stack(x)
 
 
 class Generator(nn.Module):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.linear_stack = nn.Sequential(
             nn.Linear(in_features=5, out_features=20),
@@ -33,115 +33,120 @@ class Generator(nn.Module):
             nn.Linear(in_features=20, out_features=2),
         )
 
-    def forward(self, x):
-        return self.linear_stack(x)
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        return self.linear_stack(z)
 
-def sample_z(n_samples: int):
+def sample_z(n_samples: int) -> torch.Tensor:
     return torch.normal(mean=0, std=1, size=(n_samples, 5))
 
-def sample_x(n_samples: int):
+def sample_x(n_samples: int) -> torch.Tensor:
     return torch.normal(mean=2, std=0.5, size=(n_samples, 2))
 
 
-discriminator = Discriminator()
-generator = Generator()
+def main() -> None:
+    discriminator = Discriminator()
+    generator = Generator()
 
-# Generate some initial data to see how an untrained model behaves.
-z_to_plot = sample_z(n_samples=100)
-real_data_sample = sample_x(n_samples=100)
-pre_training_generations = generator.forward(z_to_plot).detach().numpy()
+    # Generate some initial data to see how an untrained model behaves.
+    z_to_plot = sample_z(n_samples=100)
+    real_data_sample = sample_x(n_samples=100)
+    with torch.no_grad:
+        pre_training_generations = generator(z_to_plot).detach().numpy()
 
-batch_size = 128
-num_iterations = 50000
-learning_rate = 0.00005
-optimizer_g = optim.Adam(params=generator.parameters(),
-                         lr=learning_rate,
-                         weight_decay=0)
-optimizer_d = optim.Adam(params=discriminator.parameters(),
-                         lr=learning_rate,
-                         weight_decay=0)
-bce_loss = nn.BCELoss()
-g_losses, d_losses = [], []
-for iteration in range(num_iterations):
-    # Optimize the discriminator
-    optimizer_d.zero_grad()
-    z = sample_z(n_samples=batch_size)
-    x = sample_x(n_samples=batch_size)
-    # Detach tensors to avoid gradient accumulation on the generator params.
-    generated_x = generator.forward(z).detach()
+    batch_size = 128
+    num_iterations = 50000
+    learning_rate = 0.00005
+    optimizer_g = optim.Adam(params=generator.parameters(),
+                             lr=learning_rate,
+                             weight_decay=0)
+    optimizer_d = optim.Adam(params=discriminator.parameters(),
+                             lr=learning_rate,
+                             weight_decay=0)
+    bce_loss = nn.BCELoss()
+    g_losses, d_losses = [], []
+    for iteration in range(num_iterations):
+        # Optimize the discriminator
+        optimizer_d.zero_grad()
+        z = sample_z(n_samples=batch_size)
+        x = sample_x(n_samples=batch_size)
+        # Detach tensors to avoid gradient accumulation on the generator params.
+        generated_x = generator(z).detach()
 
-    d_real_pred = discriminator.forward(x)
-    d_fake_pred = discriminator.forward(generated_x)
+        d_real_pred = discriminator(x)
+        d_fake_pred = discriminator(generated_x)
 
-    real_loss = bce_loss(d_real_pred, torch.ones(batch_size, 1))
-    fake_loss = bce_loss(d_fake_pred, torch.zeros(batch_size, 1))
-    discriminator_loss = (real_loss + fake_loss) / 2
+        real_loss = bce_loss(d_real_pred, torch.ones(batch_size, 1))
+        fake_loss = bce_loss(d_fake_pred, torch.zeros(batch_size, 1))
+        discriminator_loss = (real_loss + fake_loss) / 2
 
-    discriminator_loss.backward()
-    optimizer_d.step()
+        discriminator_loss.backward()
+        optimizer_d.step()
 
-    # Optimize the generator
-    optimizer_g.zero_grad()
-    z = sample_z(n_samples=batch_size)
-    generated_x = generator.forward(z)
-    d_fake_pred = discriminator.forward(generated_x)
-    # According to Goodfellow et al. Minimizing log(1-D(G(z))) can cause
-    # saturation. Therefore, authors suggest to maximize D(G(z)).
-    # This suggestion is equivalent to minimize -D(G(z)) as implemented below
-    generator_loss = bce_loss(d_fake_pred, torch.ones(batch_size, 1))
+        # Optimize the generator
+        optimizer_g.zero_grad()
+        z = sample_z(n_samples=batch_size)
+        generated_x = generator(z)
+        d_fake_pred = discriminator(generated_x)
+        # According to Goodfellow et al. Minimizing log(1-D(G(z))) can cause
+        # saturation. Therefore, authors suggest to maximize D(G(z)).
+        # This suggestion is equivalent to minimize -D(G(z)) as implemented below
+        generator_loss = bce_loss(d_fake_pred, torch.ones(batch_size, 1))
 
-    generator_loss.backward()
-    optimizer_g.step()
-    d_losses.append(discriminator_loss.item())
-    g_losses.append(generator_loss.item())
-    print(f"Epoch {iteration}: D_loss -> {discriminator_loss.item():.4f},"
-          f" G_loss -> {generator_loss.item():.4f}")
+        generator_loss.backward()
+        optimizer_g.step()
+        d_losses.append(discriminator_loss.item())
+        g_losses.append(generator_loss.item())
+        print(f"Iterations {iteration}: "
+              f"D_loss -> {discriminator_loss.item():.4f},"
+              f" G_loss -> {generator_loss.item():.4f}")
 
-post_training_generations = generator.forward(z_to_plot).detach().numpy()
+    post_training_generations = generator(z_to_plot).detach().numpy()
 
-plt.figure()
-plt.title("Training Losses")
-plt.plot(d_losses, label="Discriminator Loss")
-plt.plot(g_losses, label="Generator Loss")
-plt.legend()
-plt.grid()
-plt.show()
+    plt.figure()
+    plt.title("Training Losses")
+    plt.plot(d_losses, label="Discriminator Loss")
+    plt.plot(g_losses, label="Generator Loss")
+    plt.legend()
+    plt.grid()
+    plt.show()
 
-fig, axes = plt.subplots(1, 2, figsize=(12, 5))
-fig.suptitle('Generator Output Comparison', fontsize=16)
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    fig.suptitle('Generator Output Comparison', fontsize=16)
 
-# Pre-Training Plot
-axes[0].set_title("Pre-Training")
-axes[0].scatter(real_data_sample[:, 0],
-                real_data_sample[:, 1],
-                label='Real Data',
-                alpha=0.6,
-                c='blue')
-axes[0].scatter(pre_training_generations[:, 0],
-                pre_training_generations[:, 1],
-                label='Generated Data',
-                alpha=0.6,
-                c='red')
-axes[0].set_xlabel("X-axis")
-axes[0].set_ylabel("Y-axis")
-axes[0].legend()
-axes[0].grid(True)
-axes[0].set_aspect('equal', adjustable='box')
+    # Pre-Training Plot
+    axes[0].set_title("Pre-Training")
+    axes[0].scatter(real_data_sample[:, 0],
+                    real_data_sample[:, 1],
+                    label='Real Data',
+                    alpha=0.6,
+                    c='blue')
+    axes[0].scatter(pre_training_generations[:, 0],
+                    pre_training_generations[:, 1],
+                    label='Generated Data',
+                    alpha=0.6,
+                    c='red')
+    axes[0].set_xlabel("X-axis")
+    axes[0].set_ylabel("Y-axis")
+    axes[0].legend()
+    axes[0].grid(True)
+    axes[0].set_aspect('equal', adjustable='box')
 
-axes[1].set_title("Post-Training")
-axes[1].scatter(real_data_sample[:, 0],
-                real_data_sample[:, 1],
-                label='Real Data',
-                alpha=0.6,
-                c='blue')
-axes[1].scatter(post_training_generations[:, 0],
-                post_training_generations[:, 1],
-                label='Generated Data',
-                alpha=0.6,
-                c='red')
-axes[1].set_xlabel("X-axis")
-axes[1].legend()
-axes[1].grid(True)
-axes[1].set_aspect('equal', adjustable='box')
+    axes[1].set_title("Post-Training")
+    axes[1].scatter(real_data_sample[:, 0],
+                    real_data_sample[:, 1],
+                    label='Real Data',
+                    alpha=0.6,
+                    c='blue')
+    axes[1].scatter(post_training_generations[:, 0],
+                    post_training_generations[:, 1],
+                    label='Generated Data',
+                    alpha=0.6,
+                    c='red')
+    axes[1].set_xlabel("X-axis")
+    axes[1].legend()
+    axes[1].grid(True)
+    axes[1].set_aspect('equal', adjustable='box')
 
-plt.show()
+    plt.show()
+
+main()
